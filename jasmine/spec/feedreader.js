2c536e2b0568f2722af31217443ae2f3b0fb8967
jthales <thaleshenrique38@live.com>
2017-04-10 20:41:57 -0300

    att

--- jasmine/spec/feedreader.js
+++ jasmine/spec/feedreader.js
@@ -1,18 +1,5 @@
-/* feedreader.js
- *
- * This is the spec file that Jasmine will read and contains
- * all of the tests that will be run against your application.
- */
-
-/* We're placing all of our tests within the $() function,
- * since some of these tests may require DOM elements. We want
- * to ensure they don't run until the DOM is ready.
- */
 $(function() {
-    /* This is our first test suite - a test suite just contains
-    * a related set of tests. This suite is all about the RSS
-    * feeds definitions, the allFeeds variable in our application.
-    */
+
     describe('RSS Feeds', function() {
 
         it('are defined', function() {
@@ -54,19 +41,44 @@ $(function() {
         });
     });
 
-    /* TODO: Write a new test suite named "Initial Entries" */
+    describe('Initial Entries', function(){
+        beforeEach(function(done){
+            loadFeed(0, function(){
+                done();
+            });
+        });
 
-        /* TODO: Write a test that ensures when the loadFeed
-         * function is called and completes its work, there is at least
-         * a single .entry element within the .feed container.
-         * Remember, loadFeed() is asynchronous so this test will require
-         * the use of Jasmine's beforeEach and asynchronous done() function.
-         */
+        it('has at least 1 entry after loadFeed function is calles', function(done){
+            var numEntries = document.querySelector('.feed').getElementsByClassName('entry').length;
+            expect(numEntries).toBeGreaterThan(0);
+            done();
+        });
 
-    /* TODO: Write a new test suite named "New Feed Selection" */
+        it("has a entry that has a link starting with 'http(s)://'", function(done) {
+            var entries = document.querySelector('.feed').getElementsByClassName('entry-link');
+            for(var i = 0; i < entries.length; i++){
+                expect(entries[i].href).toMatch(/^(http|https):\/\//);
+            }
+            done();
+        });
+    });
 
-        /* TODO: Write a test that ensures when a new feed is loaded
-         * by the loadFeed function that the content actually changes.
-         * Remember, loadFeed() is asynchronous.
-         */
+    describe('Neww Feed Selection', function() {
+
+        var initFeedSelection;
+        beforeEach(function(done) {
+            loadFeed(0, function(){
+                initFeedSelection = document.querySelector('.feed').innerHTML;
+                loadFeed(1, function() {
+                    done();
+                });
+            });
+        });
+
+        it('changes its loaded content', function(done) {
+            var newFeedSelection = document.querySelector('.feed').innerHTML;
+            expect(initFeedSelection).not.toBe(newFeedSelection);
+            done();
+        });
+    });
 }());
